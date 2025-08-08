a1a744926e5492787890b13261c031821dbd628e
Roberto <amoreroberto85@gmail.com>
2025-08-08 21:45:51 +0300

    added routes

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World"
+
+@app.route("/about")
+def about():
+    return "This is the kitchen display system"
+
+@app.route("/orders")
+def orders():
+    return "list of orders"
+
+if __name__ == "__main__":
+    app.run(debug=True)
