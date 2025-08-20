9dec59cb53013bafd6ec79d8758aab7e7e9ddd0d
Roberto <amoreroberto85@gmail.com>
2025-08-21 00:59:20 +0300

    added woo integration, order fetching, order editing

--- order_routes.py
+++ order_routes.py
@@ -1,18 +1,21 @@
 from flask import render_template, request, redirect, url_for, Blueprint
 from extensions import db
 from models import Order, User
+from woo import wcapi
 
 orders_bp = Blueprint('orders', __name__)
 
 @orders_bp.route("/orders")
-def orders():
-    default_user = User.query.filter_by(email="roberto@gmail.com").first()
-    users = User.query.all()
-    return render_template("orders.html", default_user=default_user, users=users)
+def show_orders():
+    response = wcapi.get("orders")
+    
+    if response.status_code == 200:
+        orders = response.json()
+    else:
+        return f"Error: {response.status_code}"
 
-@orders_bp.route("/orders/<int:id>")
-def order_detail(id):
-    return f"Details for order {id}"
+    return render_template("orders.html", orders=orders)
+
 
 @orders_bp.route("/add_order", methods=["GET", "POST"])
 def add_order():
@@ -31,17 +34,13 @@ def add_order():
 
 @orders_bp.route('/edit_order/<int:id>', methods=["GET", "POST"])
 def edit_order(id):
-    order = Order.query.get_or_404(id)
-
+    order = wcapi.get(f"orders/{id}").json()
     if request.method == 'POST':
-        items = request.form.get("items")
-        total_price = request.form.get("total_price")
+        status = request.form.get('status')
+        data = {"status": status}
+        wcapi.put(f"orders/{id}", data).json()
 
-        order.items = items
-        order.total_price = total_price
-        db.session.commit()
-
-        return redirect(url_for('.orders'))
+        return redirect(url_for('.show_orders'))
     return render_template('edit_order.html', order=order)
 
 @orders_bp.route('/delete_order/<int:id>', methods=["POST"])
